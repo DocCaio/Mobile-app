f993a1cfe3c5d78e452c52026f5d003b814defc2
Caio <108128119+DocCaio@users.noreply.github.com>
2023-10-10 09:11:15 -0300

    up

---
+++ app/index.js
@@ -0,0 +1,10 @@
+let titulo = document.querySelector('h1');
+titulo.innerHTML = 'Welcome to Shh!';
+
+let paragrafo = document.querySelector('p');
+paragrafo.innerHTML = 'Ahub where whispers echo <br> loudest';
+
+
+let link = document.querySelector('a');
+link.innerHTML = 'Sign up';
+
